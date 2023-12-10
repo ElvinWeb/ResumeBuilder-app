afda50661d0dcd54eabb1e696143ace8f3e0d5d4
Elvin <elvinsrkrov@gmail.com>
2023-12-10 18:47:36 +0400

    formValidation created for Cv form datas, created validation messages, and created fetchValues function for getting repeater form values

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -27,30 +27,193 @@ $(document).ready(function () {
 });
 
 const cvForm = document.getElementById("cv-form");
+const validType = {
+  TEXT: "text",
+  TEXT_EMP: "text_emp",
+  EMAIL: "email",
+  DIGIT: "digit",
+  PHONENO: "phoneno",
+  ANY: "any",
+};
 let firstNameElem = cvForm.firstname,
   lastNameElem = cvForm.lastname,
   middleNameElem = cvForm.middlename,
   imageElem = cvForm.image,
   designationElem = cvForm.designation,
-  AddressElem = cvForm.address,
-  EmailElem = cvForm.email,
-  PhoneElem = cvForm.phoneno,
-  SummaryElem = cvForm.summary;
+  addressElem = cvForm.address,
+  emailElem = cvForm.email,
+  phoneElem = cvForm.phoneno,
+  summaryElem = cvForm.summary;
+
+const fetchValues = (attrs, ...nodeLists) => {
+  let elemsAttrsCount = nodeLists.length;
+  let elemsDataCount = nodeLists[0].length;
+  let tempDataArr = [];
+  for (let i = 0; i < elemsDataCount; i++) {
+    let dataObj = {};
+
+    for (let j = 0; j < elemsAttrsCount; j++) {
+      dataObj[`${attrs[j]}`] = nodeLists[j][i].value;
+      console.log(nodeLists[j][i].value);
+    }
+    tempDataArr.push(dataObj);
+  }
+  return tempDataArr;
+};
 
 const getUserInputs = () => {
-  console.log(
-    firstNameElem.value,
-    lastNameElem.value,
-    middleNameElem.value,
-    imageElem.value,
-    designationElem.value,
-    AddressElem.value,
-    PhoneElem.value,
-    PhoneElem.value,
-    SummaryElem.value
+  let achievementsTitleElem = document.querySelectorAll(".achieve_title"),
+    achievementsDescriptionElem = document.querySelectorAll(
+      ".achieve_description"
+    );
+
+  let expTitleElem = document.querySelectorAll(".exp_title"),
+    expOrganizationElem = document.querySelectorAll(".exp_organization"),
+    expLocationElem = document.querySelectorAll(".exp_location"),
+    expStartDateElem = document.querySelectorAll(".exp_start_date"),
+    expEndDateElem = document.querySelectorAll(".exp_end_date"),
+    expDescriptionElem = document.querySelectorAll(".exp_description");
+
+  let eduSchoolElem = document.querySelectorAll(".edu_school"),
+    eduDegreeElem = document.querySelectorAll(".edu_degree"),
+    eduCityElem = document.querySelectorAll(".edu_city"),
+    eduStartDateElem = document.querySelectorAll(".edu_start_date"),
+    eduGraduationDateElem = document.querySelectorAll(".edu_graduation_date"),
+    eduDescriptionElem = document.querySelectorAll(".edu_description");
+
+  let projTitleElem = document.querySelectorAll(".proj_title"),
+    projLinkElem = document.querySelectorAll(".proj_link"),
+    projDescriptionElem = document.querySelectorAll(".proj_description");
+
+  let skillElem = document.querySelectorAll(".skill");
+
+  firstNameElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "First Name")
   );
+  lastNameElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "Last Name")
+  );
+  middleNameElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT_EMP, "Middle Name")
+  );
+  phoneElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.PHONENO, "Phone Number")
+  );
+  emailElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.EMAIL, "Email")
+  );
+  addressElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.ANY, "Address")
+  );
+  designationElem.addEventListener("keyup", (e) =>
+    validateFormData(e.target, validType.TEXT, "Designation")
+  );
+
+  return {
+    firstname: firstNameElem.value,
+    lastname: lastNameElem.value,
+    middlename: middleNameElem.value,
+    designation: designationElem.value,
+    address: addressElem.value,
+    email: emailElem.value,
+    phoneno: phoneElem.value,
+    summary: summaryElem.value,
+    achievements: fetchValues(
+      ["achieve_title", "achieve_description"],
+      achievementsTitleElem,
+      achievementsDescriptionElem
+    ),
+    experiences: fetchValues(
+      [
+        "exp_title",
+        "exp_organization",
+        "exp_location",
+        "exp_start_date",
+        "exp_end_date",
+        "exp_description",
+      ],
+      expTitleElem,
+      expOrganizationElem,
+      expLocationElem,
+      expStartDateElem,
+      expEndDateElem,
+      expDescriptionElem
+    ),
+    educations: fetchValues(
+      [
+        "edu_school",
+        "edu_degree",
+        "edu_city",
+        "edu_start_date",
+        "edu_graduation_date",
+        "edu_description",
+      ],
+      eduSchoolElem,
+      eduDegreeElem,
+      eduCityElem,
+      eduStartDateElem,
+      eduGraduationDateElem,
+      eduDescriptionElem
+    ),
+    projects: fetchValues(
+      ["proj_title", "proj_link", "proj_description"],
+      projTitleElem,
+      projLinkElem,
+      projDescriptionElem
+    ),
+    skills: fetchValues(["skill"], skillElem),
+  };
 };
+const validateFormData = (elem, elemType, elemName) => {
+  if (elemType == validType.TEXT) {
+    if (!strRegex.test(elem.value) || elem.value.trim().length == 0) {
+      addErrMsg(elem, elemName);
+    } else {
+      removeErrMsg(elem, elemName);
+    }
+  }
+
+  if (elemType == validType.TEXT_EMP) {
+    if (!strRegex.test(elem.value) || elem.value.trim().length == 0) {
+      addErrMsg(elem, elemName);
+    } else {
+      removeErrMsg(elem, elemName);
+    }
+  }
+
+  if (elemType == validType.EMAIL) {
+    if (!emailRegex.test(elem.value) || elem.value.trim().length == 0) {
+      addErrMsg(elem, elemName);
+    } else {
+      removeErrMsg(elem, elemName);
+    }
+  }
+
+  if (elemType == validType.PHONENO) {
+    if (!digitRegex.test(elem.value) || elem.value.trim().length == 0) {
+      addErrMsg(elem, elemName);
+    } else {
+      removeErrMsg(elem, elemName);
+    }
+  }
+
+  if (elemType == validType.ANY) {
+    if (elem.value.trim().length == 0) {
+      addErrMsg(elem, elemName);
+    } else {
+      removeErrMsg(elem, elemName);
+    }
+  }
+};
+
+function addErrMsg(formElem, formElemName) {
+  formElem.nextElementSibling.innerHTML = `${formElemName} is invalid`;
+}
+function removeErrMsg(formElem) {
+  formElem.nextElementSibling.innerHTML = "";
+}
 
 const generateCV = () => {
   let userData = getUserInputs();
+  console.log(userData);
 };
