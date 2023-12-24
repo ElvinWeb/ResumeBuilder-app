002583ce6b7c5e7dd28f2824ca888beeca5957f4
Elvin <elvinsrkrov@gmail.com>
2023-12-25 02:47:48 +0400

    section three is added to the index page

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,6 +1,7 @@
 let watchBtn = document.getElementById("watch-btn");
 let videoContainer = document.querySelector(".modal-container");
 let closeBtn = document.querySelector(".modal-close");
+let navbar = document.querySelector(".navbar");
 
 watchBtn.addEventListener("click", () => {
   videoContainer.classList.add("show");
